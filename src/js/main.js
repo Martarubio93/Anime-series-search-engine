d210c0315e98648e0ca750d27882caba67c93b61
Your Name <you@example.com>
2021-12-30 20:29:10 +0100

    addClassList

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,16 @@ const submitButton = document.querySelector(".submit");
 const allResultsList = document.querySelector(".js-allresults-list");
 const favouriteList = document.querySelector(".js-favourite-list");
 
-
 let allResults = []; //Array donde guadamos todos los que busca el usuario
 let favouriteResults = []; //Array donde guardamos las series favoritas del usuario
+let data = {}; //Objeto que guardaremos en localstorage
 
+function getInfo() {
+  if (localStorage.getItem("data")) {
+    data = JSON.parse(localStorage.getItem("data"));
+    paintFavList();
+  }
+}
 
 //Function fetch to get information from Api
 function getApiInformation(event) {
@@ -50,17 +56,16 @@ function paintAllResults() {
 
 //El elemento clicado se guardará en el array favouriteResults
 function addToFavList(event) {
-  const filmChangeColor = event.currentTarget;//para anadir clase fondo
-  const filmChangeColorTitle = event.currentTarget.contentText
   const filmSelected = event.currentTarget.dataset;
-  if (filmSelected === filmSelected) {
-    
-    favouriteResults.push(filmSelected);
-    
-    filmChangeColor.classList.toggle('fav')
-
-    paintFavList();
+  const filmChangeColor = event.currentTarget; //acceder al currentTarget para cambiar el color del borde
+  filmChangeColor.classList.toggle("fav"); //Cambia de color al clicar
+  if (filmChangeColor.classList.contains("fav")) {
+    favouriteResults.push(filmSelected); //Si tiene esa clase lo pushea al array de favoritos
+  } else {
+    favouriteResults.pop(filmSelected); //Si no la tiene la elimina para que no se duplique
   }
+
+  paintFavList();
 }
 
 //Pintamos lo que hay en el array favouriteResults
@@ -69,36 +74,29 @@ function paintFavList() {
   for (let index = 0; index < favouriteResults.length; index++) {
     const favElement = favouriteResults[index].name;
     const favImg = favouriteResults[index].img;
+    const favData = {
+      name: favElement,
+      img: favImg,
+    };
     if (favImg !== null) {
       favouriteList.innerHTML += `<li class="favourite_container__list--item"> <img class="favourite_container__list--img" src="${favImg}"><h2 class="favourite_container__list--h2"> ${favElement} </h2><i class="fas fa-times-circle icon" ></i></li>`;
     } else {
-      favouriteList.innerHTML += `<li class="favourite_container__list--item"> <img class="favourite_container__list--img" src="https://via.placeholder.com/210x295/ffffff/666666/?" alt="Imagen no encontrada" <h2 class="results_container__list--h2"> </h2>  <i class="fas fa-times-circle icon"></i></li>`;
+      favouriteList.innerHTML += `<li class="favourite_container__list--item"> <img class="favourite_container__list--img" src="https://via.placeholder.com/210x295/ffffff/666666/?" alt="Imagen no encontrada" <h2 class="results_container__list--h2"> </h2>  <i class="fas fa-window-close"></i></li>`;
     }
+    localStorage.setItem("data", JSON.stringify(favData));
   }
   const deleteIcons = document.querySelectorAll(".icon");
   for (const eachDeleteIcon of deleteIcons) {
     eachDeleteIcon.addEventListener("click", deleteElementFromFavList);
   }
 }
-//Function para buscar el índice de el elemento seleccionado y eliminarlo 
-function deleteElementFromFavList(){
-    const findFilmSelected = favouriteResults.findIndex(
-        (row) => row.dataset === true
-      );
-    const convertedFilmSelected= JSON.parse(JSON.stringify(findFilmSelected))
-      favouriteResults.splice(convertedFilmSelected, 1);
-      console.log(convertedFilmSelected)
-    }
 
+//Function reset (Elimina los todos los resultados)
 
-//Function reset (Elimina los resultados)
-
-function resetAllResults(event){
- event.preventDefault();
- allResultsList.innerHTML = ""; //Vaciamos lista
- allResults.splice(0, allResults.length); //Eliminamos lista de array y empezamos de nuevo
+function resetAllResults(event) {
+  event.preventDefault();
+  allResultsList.innerHTML = ""; //Vaciamos lista
+  allResults.splice(0, allResults.length); //Eliminamos lista de array y empezamos de nuevo
 }
 
-
-
-resetButton.addEventListener('click', resetAllResults)
+resetButton.addEventListener("click", resetAllResults);
