8218e37d307a1d021ded07a84795520bb03df093
Your Name <you@example.com>
2022-01-04 13:16:03 +0100

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -69,12 +69,8 @@ function addToFavList(event) {
   if (filmChangeColor.classList.contains("fav")) {
     favouriteResults.push(filmSelected); //Si tiene esa clase lo pushea al array de favoritos
   } else {
-    //favouriteResults.pop(filmSelected); //Si no la tiene la elimina para que no se duplique
-    const findFav = favouriteResults.findIndex( (item ) => item.name === filmSelected.name)
-    console.log(favouriteResults);
-    console.log(filmSelected);
-    favouriteResults.splice(findFav,1)
-    console.log(findFav)
+    favouriteResults.pop(filmSelected); //Si no la tiene la elimina para que no se duplique
+ 
   }
 
   localStorage.setItem("data", JSON.stringify(favouriteResults));
